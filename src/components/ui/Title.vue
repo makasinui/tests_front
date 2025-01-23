9aad666f0254ec2b4637145e26434de2fc437cae
makasinui <mks.shi@ya.ru>
2025-01-23 21:02:10 +0300

    feat: title component, home page create mock filter

---
+++ src/components/ui/Title.vue
@@ -0,0 +1,5 @@
+<template>
+    <h2 class="text-primary text-xl font-bold">
+        <slot></slot>
+    </h2>
+</template>
--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,3 +1,17 @@
 <template>
-    <div>Home</div>
+    <Title>Тесты онлайн</Title>
+    <Select placeholder="Категории" :items="mockCategories" />
 </template>
+
+<script lang="ts" setup>
+const mockCategories = [
+    {
+        id: 1,
+        name: 'Custom...'
+    },
+    {
+        id: 2,
+        name: 'Custom...'
+    }
+]
+</script>
