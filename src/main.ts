3af4d50f16f0fdfbe4a0bda087fc0db4c3294f6c
makasinui <mks.shi@ya.ru>
2025-01-23 21:33:34 +0300

    feat: add spinner

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,15 @@ import App from './App.vue'
 import router from './router';
 import { Title, Select } from './components';
 import { ClickOutside } from './directives';
+import Spinner from './components/ui/Spinner.vue';
 
 const app = createApp(App);
 app.use(router);
+
 app.component('Icon', Icon);
 app.component('Title', Title);
 app.component('Select', Select);
+app.component('Spinner', Spinner);
+
 app.directive('click-outside', ClickOutside);
-app.mount('#app')
+app.mount('#app');
