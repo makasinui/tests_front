276aeeee2b7feeff7240c0faa7d68177ce59170c
makasinui <mks.shi@ya.ru>
2025-01-27 18:12:05 +0300

    fix: prettier

--- src/components/quizes/QuizAnswer.vue
+++ src/components/quizes/QuizAnswer.vue
@@ -1,19 +1,27 @@
 <template>
     <div v-if="type === Questions.EType.CHOICE">
         <form class="flex flex-col gap-2">
-            <Checkbox 
-                v-for="item in localAnswers" 
+            <Checkbox
+                v-for="item in localAnswers"
                 :key="item.id"
                 :id="item.id"
                 :label="item.name"
                 :model-value="item.checked"
-                @update:modelValue="onChecked($event, item.id)"
-            />
+                @update:modelValue="onChecked($event, item.id)" />
         </form>
     </div>
-    <form class="flex flex-col gap-2 px-4" v-else>
-        <label for="text-input" class="text-center">{{ answers[0].name }}</label>
-        <textarea @input="(ev) => onChecked(ev.target.value, answers[0].id)" id="text-input" type="text" />
+    <form
+        class="flex flex-col gap-2 px-4"
+        v-else>
+        <label
+            for="text-input"
+            class="text-center"
+            >{{ answers[0].name }}</label
+        >
+        <textarea
+            @input="(ev) => onChecked((ev.target as HTMLTextAreaElement).value, answers[0].id)"
+            id="text-input"
+            type="text" />
     </form>
 </template>
 
@@ -22,13 +30,13 @@ import { Questions, type Answers } from '@/types';
 import { ref } from 'vue';
 
 interface IProps {
-    type: Questions.EType
-    answers: Answers.IItemFiltered[]
+    type: Questions.EType;
+    answers: Answers.IItemFiltered[];
 }
 
 type TEmits = {
-    change: [id: number, value: boolean | string]
-}
+    change: [id: number, value: boolean | string];
+};
 
 const props = defineProps<IProps>();
 const emit = defineEmits<TEmits>();
@@ -36,16 +44,16 @@ const emit = defineEmits<TEmits>();
 const localAnswers = ref(props.answers);
 
 const onChecked = (val: boolean | string, id: number) => {
-    if(props.type === Questions.EType.CHOICE) {
-        const answer = localAnswers.value.find(item => item.id === id)!;
-        if(val) {
+    if (props.type === Questions.EType.CHOICE) {
+        const answer = localAnswers.value.find((item) => item.id === id)!;
+        if (val) {
             localAnswers.value.forEach((item) => {
                 item.checked = false;
-            })
+            });
         }
-        answer.checked = !!val
+        answer.checked = !!val;
     }
 
-    emit('change', id, val)
-}
+    emit('change', id, val);
+};
 </script>
