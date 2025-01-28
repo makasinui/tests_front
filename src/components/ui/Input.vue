9842a13f2b5f99315fde66f1e4f2fc2b109ada2d
makasinui <mks.shi@ya.ru>
2025-01-28 18:24:00 +0300

    feat: input component error and required props add

--- src/components/ui/Input.vue
+++ src/components/ui/Input.vue
@@ -1,25 +1,43 @@
 <template>
-    <div class="flex flex-col gap-1">
-        <span>{{ label }}</span>
-        <input 
-            class="p-2 rounded-md border" type="text"
+    <div class="flex flex-col gap-1 relative">
+        <span class="relative">
+            {{ label }}
+        </span>
+        <input
+            class="relative p-2 rounded-md border"
+            type="text"
             :value="modelValue"
             :placeholder="placeholder ?? 'Введите значение'"
-            @input="emit('update:modelValue', ($event.target as HTMLInputElement).value)"
-        />
+            @input="emit('update:modelValue', ($event.target as HTMLInputElement).value)" />
+        <div
+            class="absolute -right-1.5 top-5"
+            v-if="required"
+        >
+            <Icon
+                width="12"
+                height="12"
+                icon="mdi:required"
+                class="text-red-700"
+            />
+        </div>
+        <ErrorField :error="error?.[0]?.$message" />
     </div>
 </template>
 
 <script lang="ts" setup>
+import type { ErrorObject } from '@vuelidate/core';
+
 interface IProps {
-    label: string
-    placeholder?: string
-    modelValue: string
-};
+    label: string;
+    modelValue: string;
+    placeholder?: string;
+    error?: ErrorObject[];
+    required?: boolean;
+}
 
 type TEmits = {
-    'update:modelValue': [value: string]
-}
+    'update:modelValue': [value: string];
+};
 
 const emit = defineEmits<TEmits>();
 defineProps<IProps>();
